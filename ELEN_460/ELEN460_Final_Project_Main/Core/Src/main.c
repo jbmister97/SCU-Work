8d610fbd3258154d56468cd613a0020315aac601
Justin Beltran <jbmister97@gmail.com>
2021-11-26 13:11:16 -0800

    Display test working

--- ELEN_460/ELEN460_Final_Project_Main/Core/Src/main.c
+++ ELEN_460/ELEN460_Final_Project_Main/Core/Src/main.c
@@ -30,7 +30,7 @@
     
 // Button GPIO defined in main.h
 #define COIL_PIN                GPIOB, GPIO_PIN_6
-#define LED_PIN                 GPIOA, GPIO_PIN_11
+#define LED_PIN                  GPIOA, GPIO_PIN_11
 #define GOAL_1_PIN              GPIOB, GPIO_PIN_7
 #define MOTOR_AIN1_PIN          GPIOB, GPIO_PIN_5
 #define MOTOR_AIN2_PIN          GPIOB, GPIO_PIN_4
@@ -40,6 +40,53 @@
 #define MOTOR_FWD               1
 #define MOTOR_REV               2
 #define MOTOR_SPEED             65535
+
+// I2C Device Addresses
+//#define BOARD_ADDR              0x60
+#define DISPLAY_1_ADDR          (0x70 << 1)
+#define DISPLAY_2_ADDR          (0x71 << 1)
+#define DISPLAY_3_ADDR          (0x72 << 1)
+#define DISPLAY_4_ADDR          (0x73 << 1)
+
+// Display system oscillator setup
+#define DISPLAY_OSC_OFF                 0x20
+#define DISPLAY_OSC_ON                  0x21
+#define DISPLAY_OFF                     0x80
+#define DISPLAY_ON                      0x81
+#define DISPLAY_COM0_1                  0x00
+#define DISPLAY_COM0_2                 0x01
+#define DISPLAY_COM1_1                  0x02
+#define DISPLAY_COM1_2                  0x03
+#define DISPLAY_COM2_1                  0x04
+#define DISPLAY_COM2_2                  0x05
+#define DISPLAY_COM3_1                  0x06
+#define DISPLAY_COM3_2                  0x07
+#define DISPLAY_COM4_1                  0x08
+#define DISPLAY_COM4_2                  0x09
+#define DISPLAY_COM5_1                  0x0A
+#define DISPLAY_COM5_2                  0x0B
+#define DISPLAY_COM6_1                  0x0C
+#define DISPLAY_COM6_2                  0x0D
+#define DISPLAY_COM7_1                  0x0E
+#define DISPLAY_COM7_2                  0x0F
+
+// Display pins
+#define A                        (1 << 0)
+#define B                        (1 << 1)
+#define C                        (1 << 2)
+#define D                        (1 << 3)
+#define E                        (1 << 4)
+#define F                        (1 << 5)
+#define G1                         (1 << 6)
+#define G2                        (1 << 7)
+#define H                        (1 << 0)
+#define J                        (1 << 1)
+#define K                        (1 << 2)
+#define L                        (1 << 3)
+#define M                        (1 << 4)
+#define N                        (1 << 5)
+#define DP                         (1 << 6)
+
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -53,6 +100,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c2;
+
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim17;
 
@@ -60,6 +109,15 @@ TIM_HandleTypeDef htim17;
 uint8_t keyCode = NO_KEY_PRESSED;
 uint8_t buttonPressed = false;
 uint8_t motorDir = 0;
+
+// Display
+uint8_t displayAddr = DISPLAY_1_ADDR;
+uint8_t row0_7 = 0;
+uint8_t row8_15 = 0;
+uint8_t ram[4] = {DISPLAY_COM0_1,DISPLAY_COM0_2,DISPLAY_COM1_1,DISPLAY_COM1_2};
+uint8_t buffer[3];
+char arr[] = "hi";
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -67,10 +125,13 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM3_Init(void);
 static void MX_TIM17_Init(void);
+static void MX_I2C2_Init(void);
 /* USER CODE BEGIN PFP */
 void Motor_Set_State(uint8_t state);
 void Motor_Set_AIN1(uint16_t duty);
 void Motor_Set_AIN2(uint16_t duty);
+void Display_Set_Char(uint8_t *first, uint8_t *second, char character);
+void Display_Init(uint8_t addr);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -111,9 +172,30 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM3_Init();
   MX_TIM17_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_PWM_Start_IT(&htim3, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start_IT(&htim17, TIM_CHANNEL_1);
+  
+  // Display test
+  Display_Init(DISPLAY_1_ADDR);
+  for(uint8_t i = 0; i < sizeof(arr) - 1; i++) {
+    Display_Set_Char(&row0_7, &row8_15, arr[i]);
+    // Send first half
+    buffer[0] = ram[i*2];
+    buffer[1] = row0_7;
+    HAL_I2C_Master_Transmit(&hi2c2, DISPLAY_1_ADDR, buffer, 2, HAL_MAX_DELAY);
+    
+    // Send second half
+    buffer[0] = ram[(i*2)+1];
+    buffer[1] = row8_15;
+    HAL_I2C_Master_Transmit(&hi2c2, DISPLAY_1_ADDR, buffer, 2, HAL_MAX_DELAY);
+  }
+  
+  // Turn on the display
+  buffer[0] = DISPLAY_ON;
+  HAL_I2C_Master_Transmit(&hi2c2, DISPLAY_1_ADDR, buffer, 1, HAL_MAX_DELAY);
+  
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -149,6 +231,7 @@ int main(void)
       }
       */
       
+      /*
       // Motor test
       keyCode = ScanKeyboard();
       DebounceKeyCode(keyCode);
@@ -167,6 +250,7 @@ int main(void)
       else {
         Motor_Set_State(MOTOR_STOP);
       }
+      */
     }
 
     // 100mS Tasks 
@@ -205,6 +289,7 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Configure the main internal regulator output voltage
   */
@@ -233,6 +318,59 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+  /** Initializes the peripherals clocks
+  */
+  PeriphClkInit.I2c2ClockSelection = RCC_I2C2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.Timing = 0x40003EFF;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
 }
 
 /**
@@ -452,6 +590,212 @@ void Motor_Set_AIN2(uint16_t duty) {
   HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
 
+}
+
+void Display_Set_Char(uint8_t *first, uint8_t *second, char character){
+  
+  switch(character){
+  case '0':
+    *first = A | B | C | D | E | F;
+    *second = 0;
+    break;
+  case '1':
+    *first = B | C;
+    *second = 0;
+    break;
+  case '2':
+    *first = A | B | D | E | G1 | G2;
+    *second = 0;
+    break;
+  case '3':
+    *first = A | B | C | D | G1 | G2;
+    *second = 0;
+    break;
+  case '4':
+    *first = B | C | F | G1 | G2;
+    *second = 0;
+    break;
+  case '5':
+    *first = A | C | D | F | G1 | G2;
+    *second = 0;
+    break;
+  case '6':
+    *first = A | C | D | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case '7':
+    *first = A | B | C;
+    *second = 0;
+    break;
+  case '8':
+    *first = A | B | C | D | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case '9':
+    *first = A | B | C | D | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'a':
+  case 'A':
+    *first = A | B | C | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'b':
+  case 'B':
+    *first = C | D | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'c':
+  case 'C':
+    *first = A | D | E | F;
+    *second = 0;
+    break;
+  case 'd':
+  case 'D':
+    
+    break;
+  case 'e':
+  case 'E':
+    *first = A | D | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'f':
+  case 'F':
+    *first = A | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'g':
+  case 'G':
+    
+    break;
+  case 'h':
+  case 'H':
+    *first = B | C | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'i':
+  case 'I':
+    *first = A | D;
+    *second = J | M;
+    break;
+  case 'j':
+  case 'J':
+    *first = B | C | D | E;
+    *second = 0;
+    break;
+  case 'k':
+  case 'K':
+    
+    break;
+  case 'l':
+  case 'L':
+    *first = D | E | F;
+    *second = 0;
+    break;
+  case 'm':
+  case 'M':
+    *first = B | C | E | F;
+    *second = H | K;
+    break;
+  case 'n':
+  case 'N':
+    *first = B | C | E | F;
+    *second = H | N;
+    break;
+  case 'o':
+  case 'O':
+    *first = A | B | C | D | E | F;
+    *second = 0;
+    break;
+  case 'p':
+  case 'P':
+    *first = A | B | E | F | G1 | G2;
+    *second = 0;
+    break;
+  case 'q':
+  case 'Q':
+    
+    break;
+  case 'r':
+  case 'R':
+    *first = A | B | E | F | G1 | G2;
+    *second = N;
+    break;
+  case 's':
+  case 'S':
+    *first = A | C | D | F | G1 | G2;
+    *second = 0;
+    break;
+  case 't':
+  case 'T':
+    *first = A;
+    *second = J | M;
+    break;
+  case 'u':
+  case 'U':
+    *first = B | C | D | E | F;
+    *second = 0;
+    break;
+  case 'v':
+  case 'V':
+    
+    break;
+  case 'w':
+  case 'W':
+    
+    break;
+  case 'x':
+  case 'X':
+    
+    break;
+  case 'y':
+  case 'Y':
+    
+    break;
+  case 'z':
+  case 'Z':
+    
+    break;
+  }
+  
+}
+
+void Display_Init(uint8_t addr){
+  // Start internal clock
+  uint8_t buf[2] = {DISPLAY_OSC_ON,0};
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 1, HAL_MAX_DELAY);
+  
+  // Turn off all LEDs on display 1
+  buf[0] = DISPLAY_COM0_1;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  buf[0] = DISPLAY_COM0_2;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  
+  // Turn off all LEDs on display 2
+  buf[0] = DISPLAY_COM1_1;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  buf[0] = DISPLAY_COM1_2;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  
+  // Turn off all LEDs on display 3
+  buf[0] = DISPLAY_COM2_1;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  buf[0] = DISPLAY_COM2_2;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  
+  // Turn off all LEDs on display 4
+  buf[0] = DISPLAY_COM3_1;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
+  buf[0] = DISPLAY_COM3_2;
+  buf[1] = 0;
+  HAL_I2C_Master_Transmit(&hi2c2, addr, buf, 2, HAL_MAX_DELAY);
 }
 /* USER CODE END 4 */
 
